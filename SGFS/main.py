0988230ec421bf2e4b81c47e387fb71b8b2df016
Dr-Noriega <manuellandivar99@gmail.com>
2026-03-27 09:32:19 -0400

    Add main menu options to SGFS

--- SGFS/main.py
+++ SGFS/main.py
@@ -1,1 +1,11 @@
 # Archivo principal del poryecto SGFS
+print("=== SGFS - Sistema de Gestión Financiera Simple ===")
+
+print("1) Registrar transacción")
+print("2) Ver resumen financiero")
+print("3) Ver flujo de caja de el mes")
+print("4) Buscar transacciones")
+print("5) Salir")
+
+
+print("Elegí una opción")
